cb4a85028a4c2c8344211c1e5531b5516890590a
kalkidan364 <mengistukalkidan16@gmail.com>
2025-08-16 13:56:23 -0700

    Axios and Request

---
+++ netflix-clone/src/utils/requests.js
@@ -0,0 +1,14 @@
+
+const API_KEY = Process.env.REACT_APP_APT_KEY;
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&anguage-en.US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language-en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_geners=28`,
+  fetchComadyMovies: `/discover/movie?api_key=${API_KEY}&with_geners=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_geners=27`,
+  fetchRomanceMovie: `/discover/movie?api_key=${API_KEY}&with_geners=10749`,
+  fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_geners=99`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language-en.US&page=1`,
+};
+export default requests;
